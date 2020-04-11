372ca27553251ce066289fb2dd2d011a99dc4371
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-11 14:56:46 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -3,5 +3,34 @@
 $(document).ready(function(){
 
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
+
+var getIt = function(){
+$('p').on("click", function(){
+  alert ('hey!')
+})
+}
+
+var frameIt = function(){
+  $('img').on('load', function(key){
+    var img = document.querySelector('img')
+   img.className = 'tasty'
+  })
+}
+var pressIt = function(){
+  $('input').on('keydown', function(key){
+    if (key.which == 71){
+      alert('g was pressed')
+    }
+})
+}
+  
+var submitIt = function(){
+  $('form').on('submit', function(){
+    alert ('Your form is going to be submitted now.')
+  })
+}

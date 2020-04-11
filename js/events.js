e6ef97055eb01f621be058481e28b49c3b9e428e
alpha-acrobat-9971 <qsmity@gmail.com>
2020-04-11 14:31:10 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -5,3 +5,13 @@ $(document).ready(function(){
 // call functions here
 
 });
+
+var getIt = function(){
+$('p').on("click", function(){
+  alert ('hey!')
+})
+}
+
+var frameIt = function(){
+  $('')
+}
